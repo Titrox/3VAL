6c85b2e8917d7fe7b3f8f3508175a426ae5c2dc5
st188814 <st188814@stud.uni-stuttgart.de>
2025-03-17 19:28:52 +0100

    init

---
+++ frontend/src/components/Chessboard.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { TheChessboard } from 'vue3-chessboard';
+import 'vue3-chessboard/style.css';
+</script>
+
+<template>
+  <div class="main">
+
+  </div>
+</template>
+
+<style scoped>
+
+* {
+  margin: 0;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  border: none;
+  overflow-y: hidden;
+  overflow-x: hidden;
+}
+
+.main {
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  background-color: green;
+}
+
+</style>
