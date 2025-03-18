bbb77560637a87ea1db9beb1de1de7025973b81d
st188814 <st188814@stud.uni-stuttgart.de>
2025-03-18 13:07:04 +0100

    Added Chessboard and buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,21 @@
 <script setup>
+import Chessboard from "./components/Chessboard.vue";
 </script>
 
 <template>
-
+  <div class="main-container">
+    <Chessboard />
+  </div>
 </template>
 
 <style scoped>
+.main-container {
+  height: 100vh;
+  width: 100vw;
 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+}
 </style>
--- frontend/src/components/Chessboard.vue
+++ frontend/src/components/Chessboard.vue
@@ -1,11 +1,90 @@
 <script setup>
-
+import { TheChessboard } from 'vue3-chessboard';
+import 'vue3-chessboard/style.css';
 </script>
 
 <template>
 
+  <div class="main-container">
+
+    <div class="button-container button-container--settings">
+      <button class="button button--toggle-ai">Toggle AI</button>
+    </div>
+
+    <div class="chessboard-container">
+      <TheChessboard />
+    </div>
+
+    <div class="button-container button-container--options">
+      <button class="button button--reset">Zurücksetzen</button>
+      <button class="button button--undo">Zurück</button>
+      <button class="button button--forward">Weiter</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+
+/* CONTAINER */
+
+.main-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+
+  gap: 3vw;
+
+  background-color: #add39b;
+}
+
+
+.button-container {
+  height: auto;
+  width: 20%;
+  padding: 5vh 1vw;
+
+  background-color: beige;
+  border-radius: 10px;
+  box-shadow: rgba(0,0,0,10%) 4px 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 5vh;
+}
+
+
+.chessboard-container {
+  box-shadow: rgba(0,0,0,10%) 4px 4px;
+  padding: 1vw 1vw;
+  background-color: beige;
+  border-radius: 10px;
+}
+
+/* BUTTON */
+
+.button {
+  border: none;
+  border-radius: 10px;
+  height: 5vh;
+  width: 10vw;
+
+  box-shadow: black 4px 4px;
+  background-color: #99f381;
+}
+
+
+.button:hover {
+  transform: translateY(4px);
+  box-shadow: none;
+  cursor: pointer;
+}
+
+
+.button--reset {
+  background-color: #f64040;
+}
 
 </style>
