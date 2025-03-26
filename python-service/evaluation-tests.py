b831595d9388b1f3d5a5ca27924aec7750b6d32c
st188814 <st188814@stud.uni-stuttgart.de>
2025-03-26 03:07:35 +0100

    added edgecase

--- python-service/evaluation-tests.py
+++ python-service/evaluation-tests.py
@@ -1,4 +1,4 @@
-import constants
+
 import validation
 import unittest
 
@@ -16,9 +16,9 @@ class PsqtTests(unittest.TestCase):
 
 
     def test_case_equal_3(self):
-        self.assertEqual(int(validation.calcPieceValueWithPSQT("r1b1kb1r/ppp2p2/2nq1n2/3pp1pp/PP1PP3/2NQ1N2/2P2PPP/R1B1KB1R b KQkq - 0 7")),0,)
+        self.assertEqual(int(validation.calcPieceValueWithPSQT("r2qkb2/p1p1p1p1/2n1bn1r/1p1p1p1p/P1P1P1P1/R1NB1N2/1P1P1P1P/2BQK2R b Kkq - 7 8")),0,)
 
 
     def test_case_equal_4(self):
-        self.assertEqual(int(validation.calcPieceValueWithPSQT("r2qkb2/p1p1p1p1/2n1bn1r/1p1p1p1p/P1P1P1P1/R1NB1N2/1P1P1P1P/2BQK2R b Kkq - 7 8")),0,)
+        self.assertEqual(int(validation.calcPieceValueWithPSQT("8/8/8/8/8/8/8/8 b Kkq - 7 8")),0,)
 
